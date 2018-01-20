f59f540103ad520e1462f44d0cc302161b632442
Krishneel <Krishneil1@gmail.com>
2018-01-21 00:13:35 +1000

    Complete project.

---
+++ StackoverFlowX/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace StackoverFlowX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Steplist));
+            string[] Documents = System.IO.Directory.GetFiles("../../Data/");
+            string DocumentPath = Documents[0];
+            try
+            {
+                //using (var filestream = new FileStream(@"\\Data\\Steplist.xml", FileMode.Open))
+                using (var filestream = new FileStream(DocumentPath, FileMode.Open))
+                {
+                    Steplist result = (Steplist)serializer.Deserialize(filestream);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("{0} Exception file not found", e);
+            }
+            Console.WriteLine("Enter to Exit!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StackoverFlowX/Steplist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Activation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace StackoverFlowX
+{
+    [XmlRoot("StepList")]
+    public class Steplist
+    {
+        [XmlElement("Step")]
+        public List<Step> Steps { get; set; }
+    }
+    public class Step
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+        [XmlElement("Desc")]
+        public string Desc { get; set; }
+    }
+}
